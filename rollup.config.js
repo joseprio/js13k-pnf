e0c03e2d489524e7a0bd328eba952677f401ea60
Josep del Rio <peprio@gmail.com>
2021-03-19 10:02:28 +0000

    Don't use IIFE

--- rollup.config.js
+++ rollup.config.js
@@ -13,7 +13,7 @@ export default {
   input: "src/index.js",
   output: {
     file: "dist/bundle.js",
-    format: "iife", // immediately-invoked function expression — suitable for <script> tags
+    format: "cjs",
     preferConst: true,
     sourcemap: false,
   },
